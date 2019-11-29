efc51899603b68c7ea58fe126a953c114ef2aa50
fasthro <fasthro@users.noreply.github.com>
2019-11-29 19:53:47 +0800

    xmlhttp

---
+++ client/assets/script/engine/core/network/http/xml/XMLHttpClient.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: fasthro
+ * @Date: 2019-11-29 12:27:25
+ * @Description: XMLHttpClient
+ */
+export default class XMLHttpClient {
+    
+    private url: string;
+    
+    public Connect(): void {
+
+    }
+
+    public Send(): void {
+
+    }
+}
